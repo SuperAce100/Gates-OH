e933e5cfa1552fc16115bbd63d68515da94ec851
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2024-07-03 16:27:29 -0700

    New homepage + interaction selector

--- public/scripts.js
+++ public/scripts.js
@@ -1,293 +1,89 @@
+import { on } from "process";
 import app from "./firebase-config.js";
-import {
-  getDatabase,
-  ref,
-  onValue,
-  update,
-  query,
-  orderByChild,
-  equalTo,
-  get,
-} from "firebase/database";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, onValue, update } from "firebase/database";
 
-import KJUR from "jsrsasign";
+const db = getDatabase(app);
+const officesRef = ref(db, "offices");
 
-import ZoomVideo from "@zoom/videosdk";
-const RESOLUTION = { width: 1280, height: 720 };
-const sdkKey = process.env.ZOOM_SDK_KEY;
-const sdkSecret = process.env.ZOOM_SDK_SECRET;
-const client = ZoomVideo.createClient();
+onValue(officesRef, (snapshot) => {
+  const data = snapshot.val();
+  const offices = Object.values(data);
+  console.log("offices", offices);
+  displayOffices(offices);
+});
 
-const people = [];
+let interactionRef = ref(db, "globalValues");
 
-function renderPeople() {
-  const peopleContainer = document.getElementById("rows");
-  peopleContainer.innerHTML = "";
+onValue(interactionRef, (snapshot) => {
+  let data = snapshot.val();
+  let chosenType = data.interactionType;
+  console.log("chosenType", chosenType);
+  generateDropdownContent(chosenType);
+});
 
-  const auth = getAuth(app);
-  let uid = null;
-  const user = auth.currentUser;
-  if (user) {
-    uid = user.uid;
-  }
+// Function to generate the dropdown content
+function generateDropdownContent(chosenType) {
+  const interactions = ["Scale", "Blur", "Slide"];
+  const dropdownContent = document.getElementById("dropdownContent");
 
-  people.forEach((person) => {
-    const person_uid = person["user-id"];
+  // Clear existing content
+  dropdownContent.innerHTML = "";
 
-    if (person_uid === uid) {
-      console.log("Skipping current user");
-      return;
+  interactions.forEach((interaction) => {
+    const a = document.createElement("a");
+    a.href = "#";
+    a.textContent = interaction;
+    a.id = interaction.toLowerCase();
+
+    // Add selected class if it matches the interactionType
+    if (interaction.toLowerCase() === chosenType) {
+      console.log("selecting type", chosenType);
+      a.classList.add("selected");
     }
-    const personDiv = document.createElement("div");
-    personDiv.classList.add("person");
+
+    // Add click event listener to update interactionType
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      let interactionType = interaction.toLowerCase();
+      update(interactionRef, { interactionType });
+    });
+
+    dropdownContent.appendChild(a);
+  });
+}
+generateDropdownContent();
+
+function displayOffices(offices) {
+  const officesContainer = document.getElementById("rows");
+  officesContainer.innerHTML = "";
+
+  offices.forEach((office) => {
+    const officeDiv = document.createElement("div");
+    officeDiv.classList.add("person");
 
     const nameHeading = document.createElement("h3");
-    nameHeading.textContent = person.name;
+    nameHeading.textContent = office.name;
 
-    const roomParagraph = document.createElement("p");
-    roomParagraph.textContent = person.room;
+    const descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = office.description;
 
     const indicatorDiv = document.createElement("div");
     indicatorDiv.classList.add("indicator");
-    if (person.available) {
+    if (office.doorOpen) {
       indicatorDiv.classList.add("available");
     }
+
     const spacer = document.createElement("div");
     spacer.classList.add("spacer");
 
-    personDiv.appendChild(nameHeading);
-    personDiv.appendChild(roomParagraph);
-    personDiv.appendChild(spacer);
-    personDiv.appendChild(indicatorDiv);
+    officeDiv.append(nameHeading, descriptionParagraph, spacer, indicatorDiv);
+    officeDiv.id = office.urlid;
 
-    // personDiv.addEventListener("click", () => {
-    //   const db = getDatabase();
-    //   const personRef = ref(db, `users/user-${person.id}`);
-    //   let newAvailability = !person.available;
-    //   update(personRef, { available: newAvailability }).then(() => {
-    //     console.log("Availability updated!");
-    //   });
-    // });
+    // officeDiv redirects to the office page on click
+    officeDiv.addEventListener("click", () => {
+      window.location.href = `offices/${office.urlid}/visit`;
+    });
 
-    personDiv.id = `person-${person.id}`;
-
-    let outerDiv = document.createElement("div");
-
-    outerDiv.classList.add("person-container");
-    outerDiv.appendChild(personDiv);
-
-    let videoDiv = document.createElement("div");
-    videoDiv.classList.add("video-container");
-    outerDiv.appendChild(videoDiv);
-
-    if (person.available) {
-      outerDiv.classList.add("expandable");
-      videoDiv.addEventListener("mouseover", () => {
-        // connectToVideo(person, videoDiv);
-      });
-      videoDiv.addEventListener("mouseout", () => {
-        // leaveVideo(person, videoDiv);
-      });
-    }
-
-    peopleContainer.appendChild(outerDiv);
+    officesContainer.appendChild(officeDiv);
   });
-}
-
-function connectToVideo(person, container) {
-  // connect to person's video as yourself
-
-  // get current authenticated user
-  const auth = getAuth(app);
-  let uid = null;
-  // get current user's uid without OnAuthStateChanged
-  const user = auth.currentUser;
-  if (user) {
-    uid = user.uid;
-  }
-
-  // get user's name from db
-  const usersRef = ref(db, "users");
-  const userQuery = query(usersRef, orderByChild("user-id"), equalTo(uid));
-  get(userQuery)
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        const userData = snapshot.val();
-        const key = Object.keys(userData)[0];
-        const username = userData[key].preferredName;
-        const sessionName = person.room;
-        const sessionPasscode = "";
-        container.innerHTML = "Username: " + username + "<br>Room: " + sessionName;
-        joinMeeting(username, sessionName, sessionPasscode, container, false);
-        console.log("Connected to video");
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching user data:", error);
-    });
-}
-
-function leaveVideo(person, container) {
-  // leave the video
-  container.innerHTML = "";
-  // client.leave();
-}
-
-function updateUserData(data) {
-  people.length = 0;
-  for (let key in data) {
-    people.push(data[key]);
-  }
-  renderPeople();
-}
-
-const db = getDatabase();
-const userDataRef = ref(db, "users/");
-onValue(
-  userDataRef,
-  (snapshot) => {
-    const data = snapshot.val();
-    updateUserData(data);
-  },
-  (error) => {
-    console.error("Failed to read data:", error);
-  }
-);
-
-console.log("Inside of office");
-
-function updateAvailability(id, available) {
-  const personRef = ref(db, `users/${id}`);
-  update(personRef, { available }).then(() => {
-    console.log("Availability updated!");
-  });
-}
-
-function onLogout() {
-  const usersRef = ref(db, "users");
-  const userQuery = query(usersRef, orderByChild("user-id"), equalTo(uid));
-
-  get(userQuery)
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log("User data:", snapshot.val());
-        const userData = snapshot.val();
-        const key = Object.keys(userData)[0];
-
-        updateAvailability(userData[key].id, false);
-      } else {
-        console.log("No user found with the UID:", uid);
-      }
-    })
-    .catch((error) => {
-      console.log(uid);
-      console.error("Error fetching user data:", error);
-    });
-
-  client.leave(true);
-}
-
-const auth = getAuth(app);
-let uid = null;
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    uid = user.uid;
-
-    const usersRef = ref(db, "users");
-    const userQuery = query(usersRef, orderByChild("user-id"), equalTo(uid));
-
-    get(userQuery)
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log("User data:", snapshot.val());
-          const userData = snapshot.val();
-          const key = Object.keys(userData)[0];
-          document.getElementById("heading").textContent =
-            userData[key].preferredName + "'s Office";
-
-          let videoContainer = document.querySelector("video-player-container");
-
-          joinMeeting(userData[key].preferredName, "Gates-OH", "", videoContainer, true);
-
-          updateAvailability(userData[key].id, true);
-        } else {
-          console.log("No user found with the UID:", uid);
-        }
-      })
-      .catch((error) => {
-        console.log(uid);
-        console.error("Error fetching user data:", error);
-      });
-  } else {
-    if (uid) {
-      onLogout();
-    }
-    console.log("User is signed out.");
-    window.location.assign("/login.html");
-  }
-});
-
-window.addEventListener("beforeunload", (event) => {
-  if (auth.currentUser) {
-    onLogout();
-  }
-});
-
-function generateJWT(sdkKey, sdkSecret, sessionName, role, sessionKey, userIdentity) {
-  const iat = Math.round(new Date().getTime() / 1000) - 30;
-  const exp = iat + 60 * 60 * 2;
-  const oHeader = { alg: "HS256", typ: "JWT" };
-
-  const oPayload = {
-    app_key: sdkKey,
-    tpc: sessionName,
-    role_type: role,
-    session_key: sessionKey,
-    user_identity: userIdentity,
-    version: 1,
-    iat: iat,
-    exp: exp,
-  };
-
-  const sHeader = JSON.stringify(oHeader);
-  const sPayload = JSON.stringify(oPayload);
-  const sdkJWT = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
-  return sdkJWT;
-}
-
-// Zoom Integration
-function joinMeeting(username, sessionName, sessionPasscode, container, isHost) {
-  const jwtToken = generateJWT(
-    sdkKey,
-    sdkSecret,
-    sessionName,
-    isHost ? 1 : 0,
-    sessionPasscode,
-    username
-  );
-
-  let stream;
-
-  client
-    .init("en-US", "Global", { patchJsMedia: true })
-    .then(() => {
-      console.log("Joining ", sessionName, " with JWT token:", jwtToken, " as ", username);
-      return client.join(sessionName, jwtToken, username, sessionPasscode);
-    })
-    .then(() => {
-      stream = client.getMediaStream();
-      return stream.startVideo();
-    })
-    .then(() => {
-      const userId = client.getCurrentUserInfo().userId;
-      return stream.attachVideo(userId, RESOLUTION);
-    })
-    .then((userVideo) => {
-      container.appendChild(userVideo);
-    })
-    .catch((error) => {
-      console.error("Error joining meeting:", error);
-    });
 }
