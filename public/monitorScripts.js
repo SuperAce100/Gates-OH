03f07cb574f9c185c07d710ba86c436c9621ce5c
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2024-08-16 13:47:07 -0700

    monitor tutorial disappears

--- public/monitorScripts.js
+++ public/monitorScripts.js
@@ -114,6 +114,8 @@ document.addEventListener("AcceptedPermissions", async function () {
         let visitLog = await generateVisitLog(document.getElementById("label"));
         console.log("VisitLog: ", visitLog);
 
+        document.getElementById("visitor-tutorial").style.opacity = `0`;
+
         document.getElementById("label").classList.remove("monitor-large");
         visitorId = null; // Clear the visitorId
         document.getElementById("my-video-container").classList.add("monitor-video-hidden");
