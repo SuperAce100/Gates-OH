59692f281367bab69d1398d03c20bad5ac76a689
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2024-08-21 14:25:48 -0700

    Small bug fixes

--- public/scripts.js
+++ public/scripts.js
@@ -9,98 +9,104 @@ let currentUserID = "";
 
 const auth = getAuth(app);
 
-// Function to get the current user, returns a promise
-function getCurrentUser() {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      unsubscribe(); // Unsubscribe immediately after getting the user
-      resolve(user);
-    }, reject);
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to get the current user, returns a promise
+  function getCurrentUser() {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = auth.onAuthStateChanged((user) => {
+        unsubscribe(); // Unsubscribe immediately after getting the user
+        resolve(user);
+      }, reject);
+    });
+  }
 
-getCurrentUser()
-  .then((user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log("UID", uid);
+  getCurrentUser()
+    .then((user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("UID", uid);
 
-      const userRef = query(ref(db, "users"), orderByChild("id"), equalTo(uid));
-      onValue(userRef, (snapshot) => {
-        const userData = snapshot.val();
-        if (userData) {
-          const userKey = Object.keys(userData)[0];
-          currentUserID = userData[userKey].id;
-          console.log("user", userData[userKey]);
+        document.getElementById("logout-button").addEventListener("click", () => {
+          window.location.href = "/logout";
+        });
 
-          onValue(officesRef, (snapshot) => {
-            const data = snapshot.val();
-            const offices = Object.values(data);
-            console.log("offices", offices);
-            displayOffices(offices);
-          });
-        }
-      });
-    } else {
-      console.log("No user is signed in.");
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching the current user: ", error);
-  });
+        const userRef = query(ref(db, "users"), orderByChild("id"), equalTo(uid));
+        onValue(userRef, (snapshot) => {
+          const userData = snapshot.val();
+          if (userData) {
+            const userKey = Object.keys(userData)[0];
+            currentUserID = userData[userKey].id;
+            console.log("user", userData[userKey]);
 
-function displayOffices(offices) {
-  const officesContainer = document.getElementById("rows");
-  officesContainer.innerHTML = "";
+            onValue(officesRef, (snapshot) => {
+              const data = snapshot.val();
+              const offices = Object.values(data);
+              console.log("offices", offices);
+              displayOffices(offices);
+            });
+          }
+        });
+      } else {
+        console.log("No user is signed in.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching the current user: ", error);
+    });
 
-  offices.forEach((office) => {
-    if (office.urlid === currentUserID) {
-      console.log(office.urlid, currentUserID);
-      return;
-    }
-    const officeDiv = document.createElement("div");
-    officeDiv.classList.add("person");
+  function displayOffices(offices) {
+    const officesContainer = document.getElementById("rows");
+    officesContainer.innerHTML = "";
 
-    const nameHeading = document.createElement("h3");
-    nameHeading.textContent = office.name;
+    offices.forEach((office) => {
+      if (office.urlid === currentUserID) {
+        console.log(office.urlid, currentUserID);
+        return;
+      }
+      const officeDiv = document.createElement("div");
+      officeDiv.classList.add("person");
 
-    const descriptionParagraph = document.createElement("p");
-    descriptionParagraph.textContent = office.description;
+      const nameHeading = document.createElement("h3");
+      nameHeading.textContent = office.name;
 
-    const indicatorDiv = document.createElement("div");
-    indicatorDiv.classList.add("indicator");
-    if (office.doorOpen) {
-      indicatorDiv.classList.add("available");
+      const descriptionParagraph = document.createElement("p");
+      descriptionParagraph.textContent = office.description;
 
-      // officeDiv redirects to the office page on click
-      officeDiv.addEventListener("click", () => {
-        window.location.href = `/offices/${office.urlid}`;
-      });
-    } else {
-      officeDiv.classList.add("door-closed");
-    }
+      const indicatorDiv = document.createElement("div");
+      indicatorDiv.classList.add("indicator");
+      if (office.doorOpen) {
+        indicatorDiv.classList.add("available");
 
-    const spacer = document.createElement("div");
-    spacer.classList.add("spacer");
-    officeDiv.append(nameHeading, descriptionParagraph, spacer);
-    if (office.adminUser === currentUserID) {
-      const resetButton = document.createElement("button");
-      resetButton.textContent = "Reset";
-      resetButton.classList.add("glass-button");
-      resetButton.id = "reset-button";
-      const officeRef = ref(db, `offices/${office.urlid}`);
-      resetButton.addEventListener("click", (event) => {
-        event.stopPropagation();
+        // officeDiv redirects to the office page on click
+        officeDiv.addEventListener("click", () => {
+          window.location.href = `/offices/${office.urlid}`;
+        });
+      } else {
+        officeDiv.classList.add("door-closed");
+      }
 
-        const time = new Date().getTime();
-        update(officeRef, { resetCount: time });
-      });
-      officeDiv.append(resetButton);
-    }
-    officeDiv.append(indicatorDiv);
+      const spacer = document.createElement("div");
+      spacer.classList.add("spacer");
+      officeDiv.append(nameHeading, descriptionParagraph, spacer);
+      if (office.adminUser === currentUserID) {
+        const resetButton = document.createElement("button");
+        resetButton.textContent = "Reset";
+        resetButton.classList.add("glass-button");
+        resetButton.id = "reset-button";
+        const officeRef = ref(db, `offices/${office.urlid}`);
+        resetButton.addEventListener("click", (event) => {
+          event.stopPropagation();
 
-    officeDiv.id = office.urlid;
+          const time = new Date().getTime();
+          update(officeRef, { resetCount: time });
+        });
+        officeDiv.append(resetButton);
+      }
+      officeDiv.append(indicatorDiv);
 
-    officesContainer.appendChild(officeDiv);
-  });
-}
+      officeDiv.id = office.urlid;
+
+      officesContainer.appendChild(officeDiv);
+    });
+  }
+});
